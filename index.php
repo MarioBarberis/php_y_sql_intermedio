11b0ae6f51bf981ead841382d6cc7c83f13e774d
Mario Barberis <mariojbarberis@hotmail.com>
2023-01-20 12:52:40 -0300

    prueba

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Sistema de administracion</title>
+    <link rel="stylesheet" href="stylo.css">
+
+</head>
+<body>
+    <header>
+        <h1 class="titulo"> COLEGIO INFORMATICO </h1>
+        <h2 class="titulo"> Sistema de administracion:</h2>
+        <ul class="menu_principal">
+            <li><a href="index.php"> Cargar Alumno </a></li>
+            <li><a href="ver.php"> Ver Alumno </a></li>
+            <li><a href="#"> Salir </a></li>
+
+        </ul>
+
+    </header>
+</body>
+</html>
