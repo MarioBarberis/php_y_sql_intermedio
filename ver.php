4b2acfb4d837c4128338e918b7a8d6d3b9b8955b
Mario Barberis <mariojbarberis@hotmail.com>
2023-01-22 20:06:45 -0300

    Detalle arreglo

--- ver.php
+++ ver.php
@@ -15,7 +15,7 @@ while ($mostrar_datos = mysqli_fetch_assoc($consulta_db)) {
 
 <div class="caja_alumno">
 
- <h2> <?php  echo $mostrar_datos['nombre'] ."". $mostrar_datos['apellido']; ?></h2>
+ <h2> <?php  echo $mostrar_datos['nombre'] ." ". $mostrar_datos['apellido']; ?></h2>
 <img src="img/<?php echo $mostrar_datos['imagen'] ?>" alt="<?php  echo $mostrar_datos['nombre'];?>">
 <p>
 <?php echo $mostrar_datos['descripcion']; ?>
