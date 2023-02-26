d447e0f5b8d38244032f4f0184058a3a3f6814ff
Mario Barberis <mariojbarberis@hotmail.com>
2023-02-25 23:03:11 -0300

    1

---
+++ trabajo final/index.php
@@ -0,0 +1,33 @@
+<!-- El archivo mostrar_contenido.php deberá contener una botonera que permita realizar, finalizar y ver pedidos (registros, o la que corresponda con la temática seleccionada).
+Las opciones serán “Realizar pedido”, “Ver pedidos”, “Finalizar pedidos”. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Administracion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1 class="titulo">
+        <center> SISTEMA DE ADMINISTRACION </center>
+    </h1>
+
+    <section class="botonera">
+        <center>
+            <ul class="menu_principal">
+
+                <li><a href="cargar.php"> REALIZAR PEDIDO </a></li>
+                <li><a href="ver.php"> VER PEDIDO </a></li>
+                <li><a href="anot.php"> FINALIZAR PEDIDO </a></li>
+
+            </ul>
+        </center>
+    </section>
+
+</body>
+
+</html>
