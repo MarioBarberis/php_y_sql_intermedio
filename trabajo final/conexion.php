e94dbd1dcb4f6d90dae6e7c9c9fdb3999a3fdf63
Mario Barberis <mariojbarberis@hotmail.com>
2023-03-02 19:45:25 -0300

    ver pedido on

--- trabajo final/conexion.php
+++ trabajo final/conexion.php
@@ -5,4 +5,4 @@
 /* $conexion_db = mysqli_connect("localhost", "id19044906_alumnos", "4OPKPKIy^<rEYMOI", "id19044906_php_intermedio") or exit ("no se pudo conectar"); */
 
 
-/* $conexion_db = mysqli_connect ("localhost", "root", "", "php_intermedio") or exit ("no se pudo conectar"); */
+$conexion_db = mysqli_connect ("localhost", "root", "", "php_intermedio") or exit ("no se pudo conectar");
--- trabajo final/verpedidos.php
+++ trabajo final/verpedidos.php
@@ -3,6 +3,7 @@ Cada pedido tendrá una opción de finalizar. Esta acción modificará el valor 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,7 +11,38 @@ Cada pedido tendrá una opción de finalizar. Esta acción modificará el valor 
     <title>Sistema de Administracion</title>
     <link rel="stylesheet" href="fondo.css">
 </head>
+
 <body>
-    <p> hola </p>
+
+    <section class="contenedor_alumnos">
+
+        <?php
+
+        include("conexion.php");
+        $consulta_db = mysqli_query($conexion_db, "SELECT * FROM pedidos");
+
+        // lo siguiente te permite desmenuzar cada uno de los datos
+        while ($mostrar_datos = mysqli_fetch_assoc($consulta_db)) {
+        ?>
+
+            <div class="caja_pedido">
+
+                <h2> <?php echo $mostrar_datos['nombre'] . " " . $mostrar_datos['valor']; ?></h2>
+
+                <img src="img/<?php echo $mostrar_datos['imagen'] ?>" alt="<?php echo $mostrar_datos['nombre']; ?>" class="imga">
+
+                <p class="caract">
+                    <?php echo $mostrar_datos['descripcion']; ?>
+                </p>
+
+                <a href="eliminar.php?id=<?php echo $mostrar_datos['id']; ?>"> <button> Eliminar Pedido </button></a>
+
+            </div>
+        <?php
+        }
+        ?>
+
+    </section>
 </body>
+
 </html>
