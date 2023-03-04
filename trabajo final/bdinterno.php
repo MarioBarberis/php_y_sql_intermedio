a0a90eaa19735d47a1521105fbe2b4d1328094d8
Mario Barberis <mariojbarberis@hotmail.com>
2023-03-04 20:44:14 -0300

    pulida

--- trabajo final/bdinterno.php
+++ trabajo final/bdinterno.php
@@ -3,7 +3,7 @@
 date_default_timezone_set("America/Argentina/Buenos_Aires");
 $fecha_comen = date("d-m-Y h:i:sa");
 
-$texto = "<h4> <u>NOMBRE</u> :</h4> ". $_POST['nombre1'] ."<h4> <u>VALOR</u> :</h4>". $_POST['valor1']. " <h4> <u>COMENTARIO</u> :</h4> ". $_POST['comentario1'] . " <h4> <u>FECHA DE PUBLICACION</u> :</h4>". $fecha_comen;
+$texto = " NOMBRE:". $_POST['nombre1'] ." VALOR:". $_POST['valor1']. " COMENTARIO:". $_POST['comentario1'] . " FECHA DE PUBLICACION:". $fecha_comen ."\n";
 
 $archivo = fopen('bdtexto.txt', 'a');
 fputs($archivo, $texto);
